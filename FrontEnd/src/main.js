118be57f4d5a0fb00a92171a320b2ac82464bb4d
Config001 <chegus01.gurus@gmail.com>
2021-08-07 00:17:06 +0200

    Update main.js

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -6,6 +6,16 @@ function setFormMessage(formElement, type, messsage) {
     messsageElement.classList.add('form__message--${type}');
 }
 
+function setInputError(inputElement, messsage) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement,querySelector(".form__input-error-message").textContent = messsage;
+}
+
+function clearInputError(inputElement) {
+    inputElement,classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
@@ -28,5 +38,18 @@ document.addEventListener("DOMContentLoaded", () => {
         // Perform your AJAX/Fetch login
 
         setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+
     });
 });
